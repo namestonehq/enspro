2deea463746a249c98d7cef41b2afc555936ce59
aslobodnik <aslobodnik@gmail.com>
2024-06-11 14:46:20 -0400

    update footer colors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,19 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div className="bg-white h-16 absolute items-center w-full z-10 flex">
-      <div className="flex items-center justify-center sm:w-[800px] mx-auto">
-        <LightningBoltIcon />
-        <span className="ml-1 text-slate-500">
-          Powered By NameStone <span className="mr-2 text-slate-500">|</span>
+    <div className=" bg-neutral-900 h-16 absolute items-center w-full z-10 flex">
+      <div className="flex  items-center justify-center sm:w-[800px] mx-auto">
+        <LightningBoltIcon className=" text-orange-400" />
+        <span className="ml-1 text-slate-400">
+          Powered By{" "}
+          <Link
+            href="https://namestone.xyz"
+            target="_blank"
+            className=" hover:text-orange-400 transition-colors duration-300 ease-in"
+          >
+            NameStone
+          </Link>{" "}
+          <span className="mr-2 text-slate-500">|</span>
         </span>
         <Link href="https://twitter.com/namestonehq" target="_blank">
           <Image
