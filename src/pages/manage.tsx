facca89f96a9d8044f9879837fd1f54038037dab
aslobodnik <aslobodnik@gmail.com>
2024-10-31 10:11:36 -0400

    remove cruft and add sign

--- src/pages/manage.tsx
+++ src/pages/manage.tsx
@@ -1,15 +1,5 @@
 "use client";
 import { Button } from "../components/ui/button";
-import { Input } from "../components/ui/input";
-import { Label } from "../components/ui/label";
-import {
-  Dialog,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "../components/ui/dialog";
 import NavBar from "../components/nav-bar";
 import { Address, createPublicClient, http } from "viem";
 import { mainnet } from "viem/chains";
@@ -19,15 +9,12 @@ import { getResolver } from "@ensdomains/ensjs/public";
 import { addEnsContracts, ensSubgraphActions } from "@ensdomains/ensjs";
 import { ExclamationTriangleIcon, ArrowLeftIcon } from "@radix-ui/react-icons";
 import { EnableModal } from "../components/EnableModal";
-import { ApiKeyModal } from "../components/ApiKeyModal";
 import { useAccount, useWalletClient } from "wagmi";
 import { useRouter } from "next/navigation";
-import AddressCheck from "../components/AddressCheck";
 import Footer from "../components/Footer";
 import Image from "next/image";
 import Link from "next/link";
 import _ from "lodash";
-import toast, { Toaster } from "react-hot-toast";
 import SubnameModal from "../components/SubnameModal";
 import DomainModal from "../components/DomainModal";
 
@@ -49,10 +36,10 @@ export default function Manage() {
   const [subnames, setSubnames] = useState<Subname[]>([]);
   const [basename, setbasename] = useState(searchParams?.get("name") || "");
   const [loading, setLoading] = useState(true);
-  const [resolver, setResolver] = useState("");
+
   const [isEnable, setIsEnable] = useState(false);
   const [hasApiKey, setHasApiKey] = useState(false);
-  const [offchainNames, setOffchainNames] = useState(0);
+
   const [refetch, setRefetch] = useState(0);
   const account = useAccount();
   const router = useRouter();
@@ -76,7 +63,7 @@ export default function Manage() {
     "0x7CE6Cf740075B5AF6b1681d67136B84431B43AbD",
     "0xd17347fA0a6eeC89a226c96a9ae354F785e94241",
     "0x2291053F49Cd008306b92f84a61c6a1bC9B5CB65",
-    "0xa87361c4e58b619c390f469b9e6f27d759715125", // most current
+    "0xA87361C4E58B619c390f469B9E6F27d759715125", // most current
   ];
 
   function refetchSubnames() {
@@ -88,7 +75,7 @@ export default function Manage() {
       console.log("Fetching resolver for", basename);
       setLoading(true);
       const result = await getResolver(client, { name: basename });
-      setResolver(result as string);
+
       if (goodResolvers.includes(result || "")) {
         setIsEnable(true);
       } else {
@@ -112,10 +99,6 @@ export default function Manage() {
         const displayedData = await response.json();
 
         setSubnames([...displayedData]);
-        setOffchainNames(
-          displayedData.filter((name: Subname) => name.nameType === "offchain")
-            .length
-        );
       } else {
         console.error("Failed to fetch names");
       }
@@ -179,7 +162,7 @@ export default function Manage() {
       }
 
       const apiKeyData = await enableResponse.json();
-      console.log("API Key:", apiKeyData.message);
+      router.refresh();
     } catch (error) {
       console.error("Error during enabling domain:", error);
     }
@@ -303,37 +286,26 @@ export default function Manage() {
                     />
                   </div>
                 )}
-                {!loading && isEnable && !hasApiKey && (
-                  <GetApiKeyMessage
-                    basename={basename}
-                    address={account.address || ""}
-                    fetchSubnames={fetchSubnames}
-                  />
-                )}
-                {!loading && isEnable && !hasApiKey && (
-                  <EnterApiKeyMessage
-                    basename={basename}
-                    fetchSubnames={fetchSubnames}
-                  />
-                )}
                 {/* Get Signature */}
-                <div className="flex mb-4 text-neutral-300 text-sm rounded-lg p-3 items-center justify-between w-full h-14 bg-neutral-700">
-                  <div className="flex items-center gap-2">
-                    <ExclamationTriangleIcon
-                      className=" text-amber-300"
-                      width={16}
-                      height={16}
-                    />
-                    Sign the message to prove name ownership.
+                {isEnable && !hasApiKey && (
+                  <div className="flex mb-4 text-neutral-300 text-sm rounded-lg p-3 items-center justify-between w-full h-14 bg-neutral-700">
+                    <div className="flex items-center gap-2">
+                      <ExclamationTriangleIcon
+                        className=" text-amber-300"
+                        width={16}
+                        height={16}
+                      />
+                      Sign the message to prove name ownership.
+                    </div>
+                    <Button
+                      variant="outline"
+                      className=" border-green-500 hover:bg-emerald-500"
+                      onClick={handleSign}
+                    >
+                      Sign
+                    </Button>
                   </div>
-                  <Button
-                    variant="outline"
-                    className=" border-green-500 hover:bg-emerald-500"
-                    onClick={handleSign}
-                  >
-                    Sign
-                  </Button>
-                </div>
+                )}
                 <div>
                   {subnames.length === 0 && hasApiKey ? (
                     <div className="text-neutral-300 text-center flex-col flex mt-4">
@@ -459,312 +431,4 @@ function SubnameCard({
 
 function shortenAddress(address: Address) {
   return `${address.slice(0, 6)}...${address.slice(-4)}`;
-}
-
-function GetApiKeyMessage({
-  basename,
-  address,
-  fetchSubnames,
-}: {
-  basename: string;
-  address: string;
-  fetchSubnames: () => void;
-}) {
-  const [buttonText, setButtonText] = useState("Get Key");
-  function getApiKey() {
-    setButtonText("Getting Key...");
-    fetch(`/api/get-api-key?domain=${basename}`).then((response) => {
-      if (response.ok) {
-        console.log("API key fetched successfully");
-        setButtonText("Key Fetched");
-        fetchSubnames();
-      } else {
-        console.error("Failed to fetch API key");
-        setButtonText("Failed to fetch key");
-      }
-    });
-  }
-
-  return (
-    <div className="flex mb-4 text-indigo-500 text-sm rounded-lg p-3 items-center justify-between w-full h-14 bg-indigo-50">
-      <div className="flex items-center gap-2">
-        <ExclamationTriangleIcon width={16} height={16} />
-        Get an API key to manage subnames.
-      </div>
-      <Button onMouseDown={getApiKey}>{buttonText}</Button>
-    </div>
-  );
-}
-
-function EnterApiKeyMessage({
-  basename,
-  fetchSubnames,
-}: {
-  basename: string;
-  fetchSubnames: () => void;
-}) {
-  return (
-    <div className="flex mb-4 text-orange-500 text-sm rounded-lg p-3 items-center justify-between w-full h-14 bg-orange-50">
-      <div className="flex items-center gap-2">
-        <ExclamationTriangleIcon width={16} height={16} />
-        Managed name through namestone? Enter your API key.
-      </div>
-      <ApiKeyModal
-        basename={basename}
-        fetchSubames={fetchSubnames}
-        trigger={<Button>Enter Key</Button>}
-      />
-    </div>
-  );
-}
-
-function EditDomainModal({
-  basename,
-  trigger,
-}: {
-  basename: string;
-  trigger: React.ReactNode;
-}) {
-  const [domainInfo, setDomainInfo] = useState<DomainInfo | null>(null);
-  const [open, setOpen] = useState(false);
-  // useEffect to set domainInfo
-  useEffect(() => {
-    fetch(`/api/get-domain?domain=${basename}`).then((response) => {
-      if (response.ok) {
-        response.json().then((data) => {
-          console.log("Domain Info:", data);
-          setDomainInfo(data);
-        });
-      } else {
-        console.error("Failed to fetch domain info");
-      }
-    });
-  }, [basename]);
-
-  function saveDomainInfo() {
-    // Save domainInfo
-    fetch(`/api/edit-domain`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        ...domainInfo,
-      }),
-    }).then((response) => {
-      setOpen(false);
-      if (response.ok) {
-        console.log("Domain info saved successfully");
-        toast.success("Domain info saved successfully");
-      } else {
-        console.error("Failed to save domain info");
-        toast.error("Failed to save domain info");
-      }
-    }); // Handle any network or other errors
-  }
-
-  return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>{trigger}</DialogTrigger>
-      <DialogContent
-        className="sm:max-w-[520px] max-h-[420px] sm:max-h-full overflow-y-auto  bg-neutral-800"
-        onOpenAutoFocus={(e) => e.preventDefault()}
-      >
-        <DialogHeader>
-          <DialogTitle className="text-white mb-1 font-bold">
-            Edit Name
-          </DialogTitle>
-          <div className=" text-emerald-500 font-bold">{basename}</div>
-        </DialogHeader>
-        <div className="flex flex-col">
-          <div>
-            <div className="flex text-white text-sm font-bold mb-2 mt-2 gap-2 items-center">
-              Eth Address
-            </div>
-            <Input
-              id="address"
-              value={domainInfo?.address || ""}
-              onChange={(e) => {
-                const tempDomainInfo = _.cloneDeep(domainInfo);
-                if (!tempDomainInfo) {
-                  return;
-                }
-                tempDomainInfo.address = e.target.value;
-                setDomainInfo(tempDomainInfo);
-              }}
-              className=" bg-neutral-750 focus-visible:ring-1 transition-shadow duration-300  placeholder:text-neutral-500  text-sm font-mono text-neutral-300 rounded"
-              // disabled={name.nameType === "onchain"}
-              placeholder="0x123..."
-            />
-            <AddressCheck address={domainInfo?.address || ""} />
-          </div>
-          <div>
-            <div className="flex text-white text-sm font-bold mb-2 mt-2 gap-2 items-center">
-              Description
-            </div>
-            <Input
-              id="description"
-              value={domainInfo?.text_records?.description || ""}
-              onChange={(e) => {
-                const tempDomainInfo = _.cloneDeep(domainInfo);
-                if (!tempDomainInfo) {
-                  return;
-                }
-                tempDomainInfo.text_records.description = e.target.value;
-                setDomainInfo(tempDomainInfo);
-              }}
-              className=" bg-neutral-750 focus-visible:ring-1 transition-shadow duration-300  placeholder:text-neutral-500 text-sm  text-neutral-300 rounded"
-              // disabled={name.nameType === "onchain"}
-              placeholder="i'm a web3 developer"
-            />
-          </div>
-          <div className="mt-2">
-            <div className="flex text-white text-sm font-bold mb-2 mt-2 gap-2 items-center">
-              Avatar
-            </div>
-            <Input
-              id="avatar"
-              value={domainInfo?.text_records?.avatar || ""}
-              onChange={(e) => {
-                const tempDomainInfo = _.cloneDeep(domainInfo);
-                if (!tempDomainInfo) {
-                  return;
-                }
-                tempDomainInfo.text_records.avatar = e.target.value;
-                setDomainInfo(tempDomainInfo);
-              }}
-              className=" bg-neutral-750 focus-visible:ring-1 transition-shadow duration-300 placeholder:text-neutral-500 text-sm  text-neutral-300 rounded"
-              // disabled={name.nameType === "onchain"}
-              placeholder="url for avatar"
-            />
-          </div>
-          <div className="mt-2">
-            <div className="flex text-white text-sm font-bold mb-2 mt-2 gap-2 items-center">
-              Location
-            </div>
-            <Input
-              id="location"
-              value={domainInfo?.text_records?.location || ""}
-              onChange={(e) => {
-                const tempDomainInfo = _.cloneDeep(domainInfo);
-                if (!tempDomainInfo) {
-                  return;
-                }
-                tempDomainInfo.text_records.location = e.target.value;
-                setDomainInfo(tempDomainInfo);
-              }}
-              className=" bg-neutral-750 focus-visible:ring-1 transition-shadow duration-300 placeholder:text-neutral-500 text-sm  text-neutral-300 rounded"
-              // disabled={name.nameType === "onchain"}
-              placeholder="new york city"
-            />
-          </div>
-
-          <hr className="mt-5 mb-5 border-neutral-750" />
-          {/* LINKS */}
-          <div className="flex text-white text-sm font-bold mb-2 gap-2 items-center">
-            Links
-          </div>
-          <div className="relative mb-8">
-            <Image
-              className="absolute top-1/2 left-2 -translate-y-1/2"
-              src="icon-x.svg"
-              alt="x / twitter"
-              width={16}
-              height={16}
-            />
-            <Input
-              id="X"
-              value={domainInfo?.text_records?.["com.twitter"] || ""}
-              onChange={(e) => {
-                const tempDomainInfo = _.cloneDeep(domainInfo);
-                if (!tempDomainInfo) {
-                  return;
-                }
-                tempDomainInfo.text_records["com.twitter"] = e.target.value;
-                setDomainInfo(tempDomainInfo);
-              }}
-              className="bg-neutral-750  pl-8 text-sm focus-visible:ring-1 transition-shadow duration-300 text-neutral-300 placeholder:text-neutral-500 rounded "
-              // disabled={name.nameType === "onchain"}
-              placeholder="namestonehq"
-            />
-          </div>
-          <div className="relative mb-8">
-            <Image
-              className="absolute top-1/2 left-2 -translate-y-1/2"
-              src="icon-github.svg"
-              alt="github"
-              width={16}
-              height={16}
-            />
-            <Input
-              id="github"
-              value={domainInfo?.text_records?.["com.github"] || ""}
-              onChange={(e) => {
-                const tempDomainInfo = _.cloneDeep(domainInfo);
-                if (!tempDomainInfo) {
-                  return;
-                }
-                tempDomainInfo.text_records["com.github"] = e.target.value;
-                setDomainInfo(tempDomainInfo);
-              }}
-              className=" bg-neutral-750 text-sm pl-8 focus-visible:ring-1 transition-shadow duration-300  text-neutral-300 placeholder:text-neutral-500 rounded "
-              placeholder="resolverworks"
-            />
-          </div>
-          <div className="relative mb-8">
-            <Image
-              className="absolute top-1/2 left-2 -translate-y-1/2"
-              src="icon-discord.svg"
-              alt="discord"
-              width={16}
-              height={16}
-            />
-            <Input
-              id="discord"
-              value={domainInfo?.text_records?.["com.discord"] || ""}
-              onChange={(e) => {
-                const tempDomainInfo = _.cloneDeep(domainInfo);
-                if (!tempDomainInfo) {
-                  return;
-                }
-                tempDomainInfo.text_records["com.discord"] = e.target.value;
-                setDomainInfo(tempDomainInfo);
-              }}
-              className=" bg-neutral-750  pl-8 text-sm transition-shadow duration-300  focus-visible:ring-1  text-neutral-300 placeholder:text-neutral-500 rounded"
-              placeholder="slobo.eth"
-            />
-          </div>
-          <div className="relative mb-4">
-            <Image
-              className="absolute top-1/2 left-2 -translate-y-1/2"
-              src="icon-link.svg"
-              alt="website"
-              width={16}
-              height={16}
-            />
-            <Input
-              id="website"
-              value={domainInfo?.text_records?.url || ""}
-              onChange={(e) => {
-                const tempDomainInfo = _.cloneDeep(domainInfo);
-                if (!tempDomainInfo) {
-                  return;
-                }
-                tempDomainInfo.text_records.url = e.target.value;
-                setDomainInfo(tempDomainInfo);
-              }}
-              className=" bg-neutral-750 text-sm  pl-8  transition-shadow duration-300 focus-visible:ring-1  text-neutral-300 placeholder:text-neutral-500 rounded "
-              placeholder="www.namestone.xyz"
-            />
-          </div>
-        </div>
-
-        <DialogFooter>
-          <Button onMouseDown={saveDomainInfo} className="w-24">
-            Save
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  );
 }
